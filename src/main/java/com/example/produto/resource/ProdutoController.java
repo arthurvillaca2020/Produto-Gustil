bf995e1812e20e070896531030550d8e9cafcaa4
ArthurAlmeida <arthur.villaca.jf@hotmail.com>
2022-05-29 16:37:53 -0300

    commit inicial

---
+++ src/main/java/com/example/produto/resource/ProdutoController.java
@@ -0,0 +1,29 @@
+package com.example.produto.resource;
+
+import com.example.produto.model.Produto;
+import com.example.produto.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produto")
+public class ProdutoController {
+    ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity <Produto> inserir(@RequestBody Produto produto) {
+        produtoService.cadastrarProduto(produto);
+
+        return ResponseEntity.status(201).build();
+    }
+
+
+}
