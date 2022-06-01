0b09d9d212207c01747d758e28a3448ee845e327
goku012 <67454007+goku012@users.noreply.github.com>
2022-05-31 22:10:56 -0300

    Retirada doapi/produto

--- src/main/java/com/example/produto/resource/ProdutoController.java
+++ src/main/java/com/example/produto/resource/ProdutoController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/produto")
+@RequestMapping("")
 public class ProdutoController {
     ProdutoService produtoService;
 
