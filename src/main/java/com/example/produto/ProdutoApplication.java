c244daa87e59b8f7a427dda5aadba2ec68fda103
Caio Dutra <67245651+CaioDutra1412@users.noreply.github.com>
2022-06-18 15:20:22 -0300

    Update ProdutoApplication.java
    Adicionado anotação Feign

--- src/main/java/com/example/produto/ProdutoApplication.java
+++ src/main/java/com/example/produto/ProdutoApplication.java
@@ -3,6 +3,7 @@ package com.example.produto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProdutoApplication {
