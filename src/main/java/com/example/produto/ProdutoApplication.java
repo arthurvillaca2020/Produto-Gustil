c4e278a49f63236bee035b9d97b4735e3305f7e4
arthurvillaca2020 <67216583+arthurvillaca2020@users.noreply.github.com>
2022-06-21 21:34:16 -0300

    Update ProdutoApplication.java
    referencia para eureka

--- src/main/java/com/example/produto/ProdutoApplication.java
+++ src/main/java/com/example/produto/ProdutoApplication.java
@@ -2,9 +2,12 @@ package com.example.produto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class ProdutoApplication {
 
